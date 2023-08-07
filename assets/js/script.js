12b9cad761688a38560023e73f5b65425c3e348c
brianmyer <myer.brian@gmail.com>
2023-08-07 13:42:52 -0400

    began logging info from flightdata array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,31 @@
 let flightDataArray = [];
 let drivingDataArray = [];
+let tempVar1 = document.querySelector('#temp-var-1')
 
 //live flights for experimentation are available at :
 //https://flightaware.com/live/
 //click any plain to be taken to the flight specific info and use the icao # in the parameter below. ICAO # is 3 letter airline code + 3-5 digit flight code. Usually first thing that pops up under the name of the aircraft at the top of the page. 
 
+// airline name, iata number, status, dep city, dep airport code, dep terminal, dep gate, dep time, arr city, arr airport code, arr terminal, arr gate, arr time
+
 function fetchFlightData() {
-    fetch('https://airlabs.co/api/v9/flight?flight_icao=RPA3560&api_key=d8da3920-43a6-4206-b47b-26ea2c037a69') 
+    fetch(`https://airlabs.co/api/v9/flight?flight_iata=${tempVar1.value}&api_key=d8da3920-43a6-4206-b47b-26ea2c037a69`) 
         .then(function (response) {
             return response.json();
         })
         .then(function (flightData) {
-                flightDataArray = [flightData];
+            console.log(flightData)
+            flightDataArray = [flightData];
+            console.log(flightDataArray[0].response.airline_name)
+            console.log(flightDataArray[0].response.flight_iata)
+            console.log(flightDataArray[0].response.status)
+            console.log(flightDataArray[0].response.airline_name)
+            console.log(flightDataArray[0].response.airline_name)
+            console.log(flightDataArray[0].response.airline_name)
+            console.log(flightDataArray[0].response.airline_name)
+            console.log(flightDataArray[0].response.airline_name)
+
+
         })
         };
 
